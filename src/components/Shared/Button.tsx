489275a2ae3319bf5e6537c356983661ce2559c5
Aldar <aldar.batomunkuev@asb.co.nz>
2022-08-30 09:49:23 +1200

    tailwind regex

--- src/components/Shared/Button.tsx
+++ src/components/Shared/Button.tsx
@@ -1,23 +1,27 @@
-import React from 'react';
-import classnames from 'classnames';
-import { IShareComponent } from './ISharedComponent';
+import React from "react";
+import classnames from "classnames";
+import { IShareComponent } from "./ISharedComponent";
 
 interface IButtonProps extends IShareComponent {
-	title: string;
-	isTransparent?: boolean;
-	isSecondary?: boolean;
+  title: string;
+  isTransparent?: boolean;
+  isSecondary?: boolean;
 }
 
-export const Button: React.FC<IButtonProps> = ({ title, isTransparent, isSecondary = false }) => {
-	const twSecondary = 'bg-transparent hover:text-pink-500 ';
-	const twPrimary =
-		'text-white transition-colors bg-pink-500 border-none rounded-md hover:bg-pink-700 ';
+export const Button: React.FC<IButtonProps> = ({
+  title,
+  isTransparent,
+  isSecondary = false,
+}) => {
+  const twSecondary = "bg-transparent hover:text-pink-500 ";
+  const twPrimary =
+    "text-white transition-colors bg-pink-500 border-none rounded-md hover:bg-pink-700 ";
 
-	return (
-		<button
-			className={`px-4 py-2 text-white transition-colors bg-pink-500 border-none rounded-md hover:bg-pink-700 `}
-		>
-			{title}
-		</button>
-	);
+  return (
+    <button
+      className={`px-4 py-2 text-white transition-colors bg-pink-500 border-none rounded-md hover:bg-pink-700 bg-transparent  `}
+    >
+      {title}
+    </button>
+  );
 };
