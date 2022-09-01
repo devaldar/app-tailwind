c5abe6a442de352ebcc06a6ee8f200c7f2bbc1f8
MrGeet <aldarbatomunkuev@gmail.com>
2022-09-02 09:02:29 +1200

    images

--- src/components/Exams.tsx
+++ src/components/Exams.tsx
@@ -2,17 +2,25 @@ import React from 'react';
 import { CloudIcon } from '@heroicons/react/24/solid';
 export const Exams = () => {
 	const list = ['Azure', 'AWS', 'GCP', 'Oracle', 'IBM', 'Salesforce', 'Linux'];
+	const exams = [
+		{ name: 'Azure', logo: 'azure' },
+		{ name: 'AWS', logo: 'aws' },
+		{ name: 'GCP', logo: 'google' },
+		{ name: 'Oracle', logo: 'oracle' },
+		{ name: 'Salesforce', logo: 'salesforce' },
+	];
 	return (
-		<section className="flex justify-center mt-28 align-center">
-			<div className="w-3/4 py-10 rounded-lg max-w-7xl h-72 bg-neutral-800/50">
+		<section className="flex justify-center mt-48 align-center">
+			<div className="w-3/4 py-10 rounded-lg max-w-7xl bg-neutral-800/50">
 				<h3 className="w-full text-2xl text-center">Exams include:</h3>
 				<div className="flex flex-row flex-wrap items-center justify-center max-w-md mx-auto md:max-w-3xl md:justify-between md:flex-nowrap mt-7">
-					{list.map((item) => (
-						<div key={item} className="flex flex-col items-center">
-							<div className="flex w-16 h-16 m-4 shadow-inner shadow-pink-300 bg-neutral-900 rounded-xl ">
-								<CloudIcon className="h-10 m-auto text-md" />
+					{exams.map(({ name, logo }) => (
+						<div key={name} className="flex flex-col items-center">
+							<div className="flex w-20 h-20 p-4 m-4 shadow-inner shadow-pink-300 bg-neutral-900 rounded-xl">
+								{/* <CloudIcon className="h-10 m-auto text-md" /> */}
+								<img src={`images/logos_${logo}.png`} className="object-contain" />
 							</div>
-							<div className="">{item}</div>
+							<div className="">{name}</div>
 						</div>
 					))}
 				</div>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,14 +6,14 @@ import {
 	ServerIcon,
 } from '@heroicons/react/24/solid';
 
-import bgImg from '../assets/image 12.png';
+import bgImg from '../assets/final2.png';
 import { Button } from './Shared/Button';
 
 export const Hero: React.FC<{ name: string }> = () => {
 	return (
 		<div id="home" className="flex flex-col justify-between w-full px-10 pt-12">
-			<div className="flex flex-col justify-center w-full m-auto sm:w-3/4 max-w-7xl md:flex-row">
-				<div className="flex flex-col items-start justify-center flex-shrink-0 mb-16 md:items-start md:text-start">
+			<div className="flex flex-col justify-between w-full gap-3 m-auto sm:w-3/4 max-w-7xl md:flex-row">
+				<div className="flex flex-col items-center justify-center flex-shrink-0 mb-16 text-center md:items-start md:text-start">
 					<h1 className="max-w-xs text-5xl font-bold lg:text-6xl">
 						Get{' '}
 						<span className="text-transparent bg-red-600 bg-cover bg-gradient-to-tr from-purple-500 to-pink-500 bg-clip-text">
@@ -21,17 +21,19 @@ export const Hero: React.FC<{ name: string }> = () => {
 						</span>{' '}
 						With Ease
 					</h1>
-					<p className="text-md md:w-72">
-						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore error
-						dignissimos, molestiae ipsum optio neque.
+					<p className="mt-4 mb-6 text-md md:w-80">
+						TestMaster will help you become a Cloud Expert. Access Practice Tests for
+						multiple Cloud Certifications in one place.
 					</p>
 					{/* <button className="mt-4 font-semibold text-blue-500 transition-colors hover:text-blue-600">
 						Get Started
 					</button> */}
-					<Button twClasses="mt-4 ">Get Started</Button>
+					<Button twClasses="mt-4 px-8 py-3 text-lg shadow-[0_0_10px_0] shadow-rose-400 drop-shadow-lg">
+						Get Started
+					</Button>
 				</div>
-				<div className="flex justify-center flex-auto flex-shrink">
-					<img className="max-w-xl" src={bgImg} alt="/" />
+				<div className="flex justify-center flex-auto flex-shrink w-full h-full max-w-4xl">
+					<img className="w-full max-w-2xl" src={bgImg} alt="/" />
 				</div>
 				{/* <div
 					className="absolute flex flex-col py-8 md:min-w-[760px] bottom-[5%]
